376765dbfb5a18baeb89c929da4cf7b116837120
Monkeymurf <55458043+Monkeymurf@users.noreply.github.com>
2025-01-21 18:24:52 -0600

    Added Tilemaps with collision and some background elements for test-level

---
+++ scenes/components/hitbox_component/hitbox_component.gd
@@ -0,0 +1,7 @@
+extends Area2D
+class_name HitboxComponent
+
+var damage = 0
+
+# collision_layer = 4
+# collision_mask = 0
---
+++ scenes/player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 300.0
+const MAX_JUMP_VELOCITY = -400.0
+const GRAVITY = 900.0
+
+var is_jumping = false
+
+@onready var anim_player = $AnimationPlayer
+@onready var visuals = $Visuals
+
+func _ready():
+	velocity = Vector2.ZERO
+	
+func _apply_movement(delta):
+	if is_jumping && velocity.y >= 0:
+			is_jumping = false
+	velocity.x = 300 * Input.get_axis("move_left", "move_right")
+	move_and_slide()
+	
+func _apply_gravity(delta):
+	velocity.y += GRAVITY * delta
+
+func _handle_move_input():
+	pass
+	
+
+	
