2b48fac5c6b6d04eb423dff1a0fd81f96fbaa1ee
mig <miguelv.dev@gmail.com>
2025-01-22 16:52:29 -0500

    added projectiles, added hitbox and hurtbox interaction with basic enemy

---
+++ scenes/game_objects/projectiles/projectile.gd
@@ -0,0 +1,35 @@
+extends AnimatedSprite2D
+class_name Projectile
+
+@onready var hitbox: HitboxComponent = $HitboxComponent
+@export var is_player_projectile: bool = true
+
+@export var bullet_speed: float = 5.0
+@export var bullet_dmg: float = 5.0
+var direction: Vector2 = Vector2.ZERO
+
+func _ready():
+	hitbox.damage = bullet_dmg
+	
+	if is_player_projectile:
+		hitbox.set_collision_layer_value(3, true)
+		hitbox.set_collision_layer_value(2, false)
+	else:
+		hitbox.set_collision_layer_value(3, false)
+		hitbox.set_collision_layer_value(2, true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += direction * bullet_speed * delta
+	if not is_on_screen():
+		queue_free()
+	
+func travel(target_direction: Vector2):
+	# Set up code in case we want to be able to aim (not just left and right)	
+	#direction = (target_direction - global_position).normalized()
+	direction = target_direction
+	
+func is_on_screen() -> bool: 
+	var viewport = get_viewport_rect()
+	return viewport.has_point(global_position)
--- scenes/state_machine/player_size_state/medium_action_state.gd
+++ scenes/state_machine/player_size_state/medium_action_state.gd
@@ -1,17 +1,23 @@
 extends PlayerSizeState
 
-
+@export var bullet_scene: PackedScene
 #TODO: Set the corresponding movement variables from Resource/ Singleton (max_speed, etc)
 
 
 func _melee_attack() -> void:
-	# TODO: Play animation, generate hurtbox, call event to transition back to none action state
+	player.med_melee_hitbox.damage = 3
 	player.action_anim_player.play("med_melee_attack")
+	
 
 
 # Projectile Attack	
-func _range_attack() -> void:
+func _range_attack(target_position: Vector2) -> void:
 	player.action_anim_player.play("med_range_attack")
+	
+	var bullet: Projectile = bullet_scene.instantiate()
+	bullet.global_position = player.mid_point.global_position
+	bullet.travel(target_position * 100)
+	get_tree().root.add_child(bullet)
 	
 # Swallow Action
 func _special_attack() -> void:
