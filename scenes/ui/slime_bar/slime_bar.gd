ca85752785cb0de4eebdb23f56a09581d6f2735a
mig <miguelv.dev@gmail.com>
2025-01-27 17:41:18 -0500

    switched slime bar value setting to be event based

--- scenes/ui/slime_bar/slime_bar.gd
+++ scenes/ui/slime_bar/slime_bar.gd
@@ -2,17 +2,23 @@ extends TextureProgressBar
 
 @export var player: Player 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	GameEvents.on_range_start.connect(_on_range_start)
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	value = player.slime_health.current_health
+func _ready() -> void:
+	GameEvents.on_range_start.connect(_on_range_start)
+	call_deferred("_connect_to_health") # idk if this is correct
+	
+	
+func _connect_to_health():
+	player.slime_health.health_changed.connect(_set_slime_value)
+	_set_slime_value()
 
 func _on_range_start(range_cost: float) -> void:
 	player.slime_health.damage(range_cost)
+	
+	
+func _set_slime_value() -> void:
+	value = player.slime_health.current_health
+	
 	
 func check_slime_size() -> void:
 	if value >= 70 and not (player.current_size_state == player.large_size_state):
