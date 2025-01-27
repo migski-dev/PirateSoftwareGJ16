43c699af4f86daaa47eeb769273ff0c1131663ac
migski-dev <155409914+migski-dev@users.noreply.github.com>
2025-01-27 15:55:52 -0500

    Main menu (#8)
    * Added tileset with collision, adjusted enemy & player collision per circle bug, started enemy state machine implementation
    
    * added player.visuals scaling based on size state
    
    * main menu, options, and wip controls screens
    
    ---------
    
    Co-authored-by: Monkeymurf <55458043+Monkeymurf@users.noreply.github.com>

---
+++ scenes/ui/options_menu/options_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+signal on_back_button_pressed
+
+@onready var bgm_volume_slider: HSlider = $Panel/MarginContainer/VBoxContainer/BGMVolume
+@onready var sfx_volume_slider: HSlider = $Panel/MarginContainer/VBoxContainer/SFXVolume
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value - 100)
+	
+func _on_bgm_volume_mute_toggled(toggled_on):
+	if toggled_on:
+		bgm_volume_slider.value = 0
+	else:
+		bgm_volume_slider.value = 100
+	AudioServer.set_bus_mute(1, toggled_on)
+
+
+func _on_sfx_volume_mute_toggled(toggled_on):
+	if toggled_on:
+		sfx_volume_slider.value = 0
+	else:
+		sfx_volume_slider.value = 100
+	AudioServer.set_bus_mute(2, toggled_on)
+
+
+func _on_sfx_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value - 100)
+
+
+func _on_options_back_button_pressed():
+	on_back_button_pressed.emit()
